3025f3173b8e3347b412e95c0452d86d701e5d14
Luke O'Malley <Lukeomalley95@gmail.com>
2019-09-06 11:38:20 -0400

    converted app from function to component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Algorithmus</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Algorithmus</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
