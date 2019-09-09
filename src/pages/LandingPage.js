e8257b772508e08a63ca72291fc90114983616e8
github username <github email address>
2019-09-09 12:49:35 -0400

    added some styling for the landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -11,25 +11,53 @@ const LandingPageWrapper = styled.div`
 
   div {
     width: auto;
-    border-style: solid;
-    border-color: white;
+    /* border-style: solid; */
+    /* border-color: white; */
     width: 100%;
     margin: 5px;
     height: 100%;
   }
 
   .title {
+    position: relative;
+    text-align: center;
+    color: white;
     margin: 5px;
+  }
+
+  .footer {
+    /* align-self: */
+    text-align: center;
+  }
+
+  .about {
+    text-align: center;
+  }
+
+  .title img {
+    width: 100%;
+    height: 100%;
+  }
+
+  .centered {
+    position: absolute;
+    margin-top: 40px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 40px;
   }
 `;
 
 const LandingPage = () => {
   return (
     <LandingPageWrapper>
-      <h1 className="title">ALGORITHMUS</h1>
-      <div className="landingImg">Nice Image Here</div>
+      <div className="title">
+        <img src="https://static.vecteezy.com/system/resources/previews/000/524/720/non_2x/fantasy-wide-sci-fi-martian-background-for-ui-game-vector.jpg" />
+        <div className="centered">ALGORITHMUS</div>
+      </div>
+
       <div className="about">About Algorithmus</div>
-      <div className="loginButton">FLEX BOX FTW</div>
       <div className="footer">I am Footer</div>
     </LandingPageWrapper>
   );
