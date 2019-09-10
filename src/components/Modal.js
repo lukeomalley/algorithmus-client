1c4d0e672c15cb67b81cf84e0a6c439c06d2672f
github username <github email address>
2019-09-10 15:29:51 -0400

    styled confirmation page

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import { PrimaryButton } from "../styled-components/Buttons";
 
 const ModalWrapper = styled.div`
   display: flex;
   height: 100vh;
-  border-style: solid;
-  border-color: white;
+  /* border-style: solid;
+  border-color: white; */
   align-items: center;
   justify-content: center;
   align-content: center;
@@ -14,6 +15,15 @@ const ModalWrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    /* border-style: solid;
+    border-color: white; */
+    height: 50%;
+    width: 50%;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 1);
+  }
+
+  .modal div {
+    margin: 10px;
   }
 `;
 
@@ -33,9 +43,9 @@ export default class Modal extends React.Component {
           {this.conditionalRender()}
           <div className="content">{this.props.message}</div>
           <div className="actions">
-            <button className="toggle-button" onClick={this.props.close}>
+            <PrimaryButton className="toggle-button" onClick={this.props.close}>
               OK
-            </button>
+            </PrimaryButton>
           </div>
         </div>
       </ModalWrapper>
